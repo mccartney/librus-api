a45402bab21b8065d38c149f44af16b7c1ec7715
Grzegorz Olędzki <grzegon@poczta.onet.pl>
2017-12-18 23:04:53 +0100

    Sending emails with content of messages

--- index.js
+++ index.js
@@ -7,42 +7,53 @@ const Librus = require(".");
 
 var ses = new AWS.SES();
 
-function sendEmail(content, callback) {
+function sendEmail(subject, content, event, context) {
    var params = {
          Destination: {
                         ToAddresses: [process.env.EMAIL_TO]
                       },
          Message: {
             Body: {
-               Text: {
-                  Data: content
+               Html: {
+                  Charset: "UTF-8",
+                  Data: content + "<br/><br/>-- <br/>Sent by "+context.functionName
                }
             },
             Subject: {
-               Data: "Test 1849"
+               Data: subject
             }
          },
          Source: process.env.EMAIL_FROM
    }
    
-   ses.sendEmail(params, callback);
+   ses.sendEmail(params, 
+         function(err, data) {
+           if(err) console.log(err);
+           else {
+             context.succeed(event)
+           }
+         })
 }
+
+function inspectMessage(msg, librus, event, context) {
+  if (!msg.read) {
+    librus.inbox.getMessage(5, msg.id).then(data => {
+      var from = data.user+"("
+      var fromFriendly = from.substring(0, from.indexOf('(')).trim()
+      
+      sendEmail("Librus ("+fromFriendly+"): " + data.title, data.html, event, context)
+    });
+  }
+}
+
 
 exports.myHandler = function(event, context, callback) {
   let lib = new Librus();
   lib.authorize(process.env.LIBRUS_USER_NAME, process.env.LIBRUS_PASSWORD).then(function () {
     lib.inbox.listInbox(5).then( data => {
-      sendEmail(util.inspect(data, null, true) +"\n\n-- Sent by "+context.functionName, 
-         function(err, data) {
-           if(err) console.log(err);
-           else {
-             console.log("===EMAIL SENT===");
-             console.log(data);
-             console.log("EMAIL CODE END");
-             context.succeed(event)
-           }
-         })
-     })
+    
+      data.forEach(function(value) { return inspectMessage(value, lib, event, context)})
+   })   
   });
 
 }
